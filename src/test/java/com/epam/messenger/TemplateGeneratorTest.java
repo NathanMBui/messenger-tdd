fe4eb8657eb5cdf7594509681e236b3c7f926395
NhatBui <nhat_bui@epam.com>
2021-07-11 17:04:51 +0700

    init

---
+++ src/test/java/com/epam/messenger/TemplateGeneratorTest.java
@@ -0,0 +1,12 @@
+package com.epam.messenger;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TemplateGeneratorTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
+}
